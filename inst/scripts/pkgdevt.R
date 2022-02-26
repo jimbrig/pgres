ad83ad40c3aff0b4051c0d2073d97a4fda329e2a
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-02-25 20:02:20 -0500

    Initial commit

---
+++ inst/scripts/pkgdevt.R
@@ -0,0 +1,85 @@
+
+#  ------------------------------------------------------------------------
+#
+# Title : pgres R Package Development Script
+#    By : Jimmy Briggs
+#  Date : 2022-02-25
+#
+#  ------------------------------------------------------------------------
+
+# library R packages ------------------------------------------------------
+
+library(pkgdev)
+pkgdev::library_dev_packages()
+
+# initialize package ------------------------------------------------------
+
+usethis::create_package("pgres")
+usethis::use_namespace()
+usethis::use_roxygen_md()
+usethis::use_package_doc()
+usethis::use_git()
+usethis::use_tibble() # #' @return a [tibble][tibble::tibble-package]
+usethis::use_pipe()
+usethis::use_tidy_eval()
+devtools::document()
+
+
+# github ------------------------------------------------------------------
+
+# set description and title first so included in GH repo
+desc::desc_set(
+  "Description" = "My awesome description.",
+  "Title" = "My awesome title"
+)
+
+usethis::use_github(private = FALSE)
+
+# github labels -----------------------------------------------------------
+
+templateeR::use_gh_labels()
+
+
+# package docs ------------------------------------------------------------
+
+usethis::use_readme_rmd()
+usethis::use_mit_license()
+usethis::use_package_doc()
+usethis::use_news_md()
+
+
+# functions ---------------------------------------------------------------
+
+c(
+  # add function file names here:
+  "utils",
+  "zzz"
+) |> purrr::walk(usethis::use_r, open = FALSE)
+
+
+# tests -------------------------------------------------------------------
+
+c(
+  # add function test file names here:
+
+) |> purrr::walk(usethis::use_test)
+
+# data --------------------------------------------------------------------
+
+c(
+  # add data prep script names here:
+
+) |> purrr::walk(usethis::use_data_raw)
+
+# vignettes ---------------------------------------------------------------
+
+c(
+  # add vignette names here:
+  "pgres"
+
+) |> purrr::walk(usethis::use_vignette)
+
+
+
+
+
