11752a7e30e00836d32b8661ea3984e574eca1bd
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-02-25 21:31:25 -0500

    config: skaffold R package

--- inst/scripts/pkgdevt.R
+++ inst/scripts/pkgdevt.R
@@ -1,7 +1,7 @@
 
 #  ------------------------------------------------------------------------
 #
-# Title : pgres R Package Development Script
+# Title : `pgres` R Package Development Script
 #    By : Jimmy Briggs
 #  Date : 2022-02-25
 #
@@ -17,33 +17,35 @@ pkgdev::library_dev_packages()
 usethis::create_package("pgres")
 usethis::use_namespace()
 usethis::use_roxygen_md()
-usethis::use_package_doc()
+usethis::use_package_doc(open = FALSE)
 usethis::use_git()
 usethis::use_tibble() # #' @return a [tibble][tibble::tibble-package]
 usethis::use_pipe()
-usethis::use_tidy_eval()
+# usethis::use_tidy_eval()
 devtools::document()
 
+pkgdev::use_feedback_helpers(open = FALSE)
 
 # github ------------------------------------------------------------------
 
 # set description and title first so included in GH repo
 desc::desc_set(
-  "Description" = "My awesome description.",
-  "Title" = "My awesome title"
+  "Description" = "Suite of Database Utility Helpers Specific to PostgreSQL.",
+  "Title" = "PostgreSQL Utility Helper Functions in R"
 )
 
 usethis::use_github(private = FALSE)
 
 # github labels -----------------------------------------------------------
-
+library(templateeR)
 templateeR::use_gh_labels()
-
+templateeR::use_git_cliff()
+templateeR::use_git_cliff_action()
 
 # package docs ------------------------------------------------------------
 
 usethis::use_readme_rmd()
-usethis::use_mit_license()
+usethis::use_mit_license("Jimmy Briggs | 2022")
 usethis::use_package_doc()
 usethis::use_news_md()
 
@@ -53,7 +55,10 @@ usethis::use_news_md()
 c(
   # add function file names here:
   "utils",
-  "zzz"
+  "set_primary_key",
+  "set_foreign_key",
+  "clean_sql"#,
+  # "create_sql"
 ) |> purrr::walk(usethis::use_r, open = FALSE)
 
 
@@ -80,6 +85,26 @@ c(
 ) |> purrr::walk(usethis::use_vignette)
 
 
+# pkgdown -----------------------------------------------------------------
 
+usethis::use_pkgdown_github_pages()
+remove.packages("chameleon")
+pak::pak("thinkr-open/chameleon")
 
+fs::dir_create("inst/docs")
+chameleon::open_pkgdown_function()
 
+usethis::use_coverage()
+usethis::use_github_action("test-coverage")
+
+chameleon::open_pkgdown_function()
+attachment::att_amend_desc()
+attachment::create_dependencies_file(to = "inst/scripts/dependencies.R")
+
+covrpage::covrpage()
+
+usethis::use_lifecycle_badge("experimental")
+usethis::use_spell_check()
+
+devtools::build_readme()
+codemetar::write_codemeta()
